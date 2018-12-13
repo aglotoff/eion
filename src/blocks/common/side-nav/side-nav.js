782fbca1ec89dd933d7b43b364959cc7dc9d176a
polarfawx <andrei.glotoff@gmail.com>
2018-12-13 22:57:38 +0300

    add side navigation

---
+++ src/blocks/common/side-nav/side-nav.js
@@ -0,0 +1,30 @@
+/**
+ * @file Implementation of the side-nav block
+ */
+
+// -------------------------- BEGIN MODULE VARIABLES --------------------------
+// TODO: add code here
+// --------------------------- END MODULE VARIABLES ---------------------------
+
+// -------------------------- BEGIN UTILITY FUNCTIONS -------------------------
+// TODO: add code here
+// --------------------------- END UTILITY FUNCTIONS --------------------------
+
+// ----------------------------- BEGIN DOM METHODS ----------------------------
+// TODO: add code here
+// ------------------------------ END DOM METHODS -----------------------------
+
+// --------------------------- BEGIN EVENT HANDLERS ---------------------------
+// TODO: add code here
+// ---------------------------- END EVENT HANDLERS ----------------------------
+
+// --------------------------- BEGIN PUBLIC METHODS ---------------------------
+/**
+ * Initialize the side-nav module.
+ * @return true;
+ */
+export const initModule = function() {
+    // TODO: add code here
+    return true;
+};
+// ---------------------------- END PUBLIC METHODS ----------------------------
