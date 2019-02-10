0e8af4c7c952b693db4849c3686402fd934eb2d6
polarfawx <andrei.glotoff@gmail.com>
2019-02-10 20:49:44 +0300

    refactor the account block

--- src/blocks/common/lang-menu/lang-menu.js
+++ src/blocks/common/lang-menu/lang-menu.js
@@ -2,23 +2,106 @@
  * @file Implementation of the language menu block
  */
 
-import {makeDropdown} from '../../../js/utils';
+// -------------------------- BEGIN MODULE VARIABLES --------------------------
+const KEY_ESC   = 27;
+const KEY_SPACE = 32;
+// --------------------------- END MODULE VARIABLES ---------------------------
 
 // --------------------------- BEGIN PUBLIC METHODS ---------------------------
 /**
  * Initialize the language menu module.
- * @return true;
+ * @return true
  */
 export const initModule = function() {
-    const $langMenu = $('.lang-menu');
-    const $toggle   = $langMenu.find('.lang-menu__toggle');
+    const $menu = $('.lang-menu');
+    const $toggle = $menu.find('.lang-menu__toggle');
 
-    makeDropdown($langMenu, $toggle, {
-        hoverToggles: true,
-        onToggle(open) {
-            $langMenu.toggleClass('lang-menu_visible', open);
-        },
+    let isExpanded = false;
+
+    const toggleLangMenu = function(open) {
+        isExpanded = open;
+
+        if (open) {
+            setTimeout(() => {
+                $(document).on({
+                    'click': onOutsideLangMenuClick,
+                    'focusin': onOutsideLangMenuClick,
+                });
+                $menu.on('keydown', onLangMenuKeydown);
+            }, 0);
+        } else {
+            $(document).off({
+                'click': onOutsideLangMenuClick,
+                'focusin': onOutsideLangMenuClick,
+            });
+            $menu.on('keydown', onLangMenuKeydown);
+        }
+
+        $menu.toggleClass('lang-menu_visible', open);
+        $toggle.attr('aria-expanded', String(open));
+    };
+
+    const onOutsideLangMenuClick = function(event) {
+        const target = event.target;
+        if (!(
+            target === document
+            || $menu.is($(target))
+            || $.contains($menu.get(0), target)
+        )) {
+            toggleLangMenu(false);
+        }
+    };
+
+    const onLangMenuToggleClick = function() {
+        if (!isExpanded) {
+            toggleLangMenu(true);
+        } else {
+            toggleLangMenu(false);
+        }
+    };
+
+    const onLangMenuToggleKeydown = function(event) {
+        if (event.which === KEY_SPACE) {
+            event.preventDefault();
+            
+            if (!this._isExpanded) {
+                toggleLangMenu(true);
+            } else {
+                toggleLangMenu(false);
+            }
+        }
+    };
+
+    const onLangMenuToggleKeyup = function(event) {
+        if (event.which === KEY_SPACE) {
+            event.preventDefault();
+        }
+    };
+
+    const onLangMenuKeydown = function(event) {
+        if ((event.which === KEY_ESC) && isExpanded) {
+            toggleLangMenu(false);
+            $toggle.focus();
+            event.stopPropagation();
+        }
+    };
+
+    const onLangMenuMouseenter = function() {
+        toggleLangMenu(true);
+    };
+
+    const onLangMenuMouseleave = function() {
+        toggleLangMenu(false);
+    };
+
+    $toggle.on({
+        'click': onLangMenuToggleClick,
+        'keydown': onLangMenuToggleKeydown,
+        'keyup': onLangMenuToggleKeyup,
     });
+
+    $menu.hover(onLangMenuMouseenter, onLangMenuMouseleave);
 
     return true;
 };
+// ---------------------------- END PUBLIC METHODS ----------------------------
