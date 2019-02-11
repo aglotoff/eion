d5e8d4fe6032afff98fac1b1daa00235408a0910
polarfawx <andrei.glotoff@gmail.com>
2019-02-11 16:59:20 +0300

    refactor the side-nav block

--- src/blocks/common/side-nav/side-nav.js
+++ src/blocks/common/side-nav/side-nav.js
@@ -1,61 +1,66 @@
 /**
  * @file Implementation of the vertical menu block
+ * @author Andrey Glotov
  */
 
-import {makeModal} from '../../../js/utils';
+/* global focusTrap */
 
 // --------------------------- BEGIN PUBLIC METHODS ---------------------------
 /**
  * Initialize the vertical menu module.
- * @return true;
+ * @return true
  */
 export const initModule = function() {
-    const $page        = $('.page');
-    const $nav         = $('.side-nav');
-    const $drawer      = $nav.find('.side-nav__drawer');
-    const $close       = $drawer.find('.side-nav__close');
-    const $submenus    = $drawer.find('.side-nav__menu_submenu');
-    const $searchForm  = $drawer.find('.side-nav__search');
+    const $page = $('.page');
+    const $nav = $('.side-nav');
+    const $drawer = $nav.find('.side-nav__drawer');
+    const $close = $drawer.find('.side-nav__close');
+    const $submenus = $drawer.find('.side-nav__menu_submenu');
+    const $searchForm = $drawer.find('.side-nav__search');
     const $searchInput = $searchForm.find('.side-nav__search-input');
 
-    const closeSubmenu = function($submenu) {
+    const closeSidenavSubmenu = function($submenu) {
         $submenu
             .slideUp()
             .prev('.side-nav__menu-link')
             .attr('aria-expanded', 'false');
     };
 
-    const modalLogic = makeModal($drawer, {
-        onToggle(open) {
-            if (!open) {
-                closeSubmenu($submenus);
-            }
+    const trap = focusTrap($drawer.get(0), {
+        clickOutsideDeactivates: true,
+        escapeDeactivates: true,
+        onDeactivate: function() {
+            closeSidenavSubmenu($submenus);
 
             $nav
-                .attr('aria-hidden', String(!open))
-                .toggleClass('side-nav_visible', open);
+                .attr('aria-hidden', 'true')
+                .removeClass('side-nav_visible');
         },
     });
 
     $close.click(function closeSidenav() {
-        modalLogic.hide();
+        trap.deactivate();
     });
 
     // Listen for a global event to show the menu
     $page.on('sidenav-show', function showSidenav() {
-        modalLogic.show();
+        $nav
+            .attr('aria-hidden', 'false')
+            .addClass('side-nav_visible');
+
+        trap.activate();
     });
 
     $submenus.prev('.side-nav__menu-link').click(function toggleSubmenu() {
-        const $link       = $(this);
+        const $link = $(this);
         const $parentItem = $link.closest('.side-nav__menu-item');
 
         // If the submenu is expanded, collapse it. Otherwise, expand the
         // submenu but collapse all its siblings.
         if ($link.attr('aria-expanded') === 'true') {
-            closeSubmenu($parentItem.find('.side-nav__menu_submenu'));
+            closeSidenavSubmenu($parentItem.find('.side-nav__menu_submenu'));
         } else {
-            closeSubmenu($parentItem
+            closeSidenavSubmenu($parentItem
                 .siblings('.side-nav__menu-item')
                 .find('.side-nav__menu_submenu'));
 
@@ -64,9 +69,10 @@ export const initModule = function() {
         }
     });
 
-    $searchInput.focus(function onSearchFocus() {
+    $searchInput.focus(function onSidenavSearchFocus() {
         $searchForm.addClass('side-nav__search_focused');
-        $searchInput.blur(function onSearchBlur() {
+        
+        $searchInput.blur(function onSidenavSearchBlur() {
             $searchForm.removeClass('side-nav__search_focused');
         });
     });
